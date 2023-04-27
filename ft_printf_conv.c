5676c5eeadda8d3ea1563b702008fc52392b84ea
Alan Le Bouder <ale-boud@c2r2p4.42lehavre.fr>
2023-04-27 21:23:56 +0200

    norm

--- ft_printf_conv.c
+++ ft_printf_conv.c
@@ -6,7 +6,7 @@
 /*   By: ale-boud <ale-boud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:43:43 by ale-boud          #+#    #+#             */
-/*   Updated: 2023/04/27 21:09:46 by ale-boud         ###   ########.fr       */
+/*   Updated: 2023/04/27 20:32:59 by ale-boud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,37 +98,4 @@ int	ft_print_uns(t_printf_arg *arg)
 	written = ft_print_str(arg);
 	free(str);
 	return (written);
-}
-
-int	ft_print_hxm(t_printf_arg *arg)
-{
-	int		written;
-	char	*str;
-
-	str = ft_printf_uitoabase(arg->d.unb, "0123456789abcdef", arg);
-	arg->precflag = -1;
-	arg->d.str = str;
-	written = ft_print_str(arg);
-	free(str);
-	return (written);
-}
-
-int	ft_print_hxc(t_printf_arg *arg)
-{
-	int		written;
-	char	*str;
-
-	str = ft_printf_uitoabase(arg->d.unb, "0123456789ABCDEF", arg);
-	arg->precflag = -1;
-	arg->d.str = str;
-	written = ft_print_str(arg);
-	free(str);
-	return (written);
-}
-
-int	ft_print_prc(t_printf_arg *arg)
-{
-	arg->d.c = '%';
-	ft_putchar_fd(arg->d.c, STDOUT_FILENO);
-	return (1);
 }
